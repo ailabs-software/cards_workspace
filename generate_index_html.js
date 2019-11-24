5df654bbd5cbb4022846686a3267472f8154df62
Sheyenne York <support@bpilotglobal.com>
2019-11-24 12:58:35 -0500

    Fix bugs

---
+++ generate_index_html.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+var cardHtmlFiles = fs.readdirSync("cards_html/");
+
+var out = "";
+
+out += "<!DOCTYPE html>\n";
+out += "<html>\n";
+out += "</body>\n";
+out += '<meta charset="utf-8"/>\n';
+out += '<link class="dg5_stylesheet_link" rel="stylesheet" type="text/css" href="https://george.shopwindow.io/dg5_platform/static/dist/ShopwindowServer-server-ASSETSOUT/out.css?v=1574564583543" />\n';
+out += '<link class="dg5_stylesheet_link" rel="stylesheet" type="text/css" href="https://george.shopwindow.io/dg5_platform/static/dist/PipelineEmbedClient-client-ASSETSOUT/out.css?v=1574564583543" />\n';
+out += '<link class="dg5_stylesheet_link" rel="stylesheet" type="text/css" href="https://system-dev.shopwindow.io/dg5_platform/static/www/lordekit-static/lordekit-responsive-typography/out/web_stylesheet.css" />\n';
+out += '<link class="dg5_stylesheet_link" rel="stylesheet" type="text/css" href="sass_output.css" />\n';
+out += '<link class="dg5_stylesheet_link" rel="stylesheet" type="text/css" href="https://lordefoundry-dev.safetyhandler.com/fontfoundry2/css/Roboto" />\n';
+
+for (var i=0; i < cardHtmlFiles.length; i++)
+{
+  var cardHtmlFile = cardHtmlFiles[i];
+  var cardHtml = fs.readFileSync("cards_html/" + cardHtmlFile, "utf8");
+
+  // Parse to DOM.
+  var dom = new JSDOM(cardHtml);
+
+  var cardElement = dom.window.document.querySelector(".web-grid-layout-cell");
+
+  var cardHtml = cardElement.innerHTML;
+
+  out += "<!-- card: " + cardHtmlFile + " -->\n";
+  out += cardHtml;
+  out += "\n";
+}
+
+out += '<script type="text/javascript" src="https://george.shopwindow.io/dg5_platform/static/build/mainEntryPoint_PipelineEmbedClient_client.dart.js?v=1574564583543"></script>\n';
+out += '<script type="text/javascript">\n';
+out += 'JSINTEROP_APPLY_DECORATORS(document.body);\n';
+out += '</script>\n';
+
+out += "</body>\n";
+out += "</html>\n";
+
+fs.writeFileSync("index.html", out);
